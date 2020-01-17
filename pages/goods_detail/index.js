775561e9a9006b7f3b33c4328ecd308d37692c3d
panjianwang <TomPan2012@163.com>
2020-01-17 17:05:52 +0800

    项目首页完成

--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -1,4 +1,4 @@
-// pages/demo/demo.js
+// pages/goods_detail/index.js
 Page({
 
   /**
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,68 +4,102 @@ Page({
    * 页面的初始数据
    */
   data: {
-    searchText:"搜索",
-    nav:[
-      {id:1,name:"分类"},
-      {id:2,name:"秒杀拍"},
-      {id:3,name:"超市购"},
-      {id:4,name:"母婴品"}
-    ]
+    searchText: "搜索",
+    nav: [
+      { id: 1, name: "分类" },
+      { id: 2, name: "秒杀拍" },
+      { id: 3, name: "超市购" },
+      { id: 4, name: "母婴品" }
+    ],
+    images:[],
+    menus:[],
+    floors:[]
   },
-
+  getValue(e){
+    console.log(e.detail)
+  },
+  // 获取首页选项菜单
+  getmenu(){
+   wx.request({
+      url:"https://api.zbztb.cn/api/public/v1/home/catitems",
+      success:res=>{
+        console.log(res)
+        this.setData({
+          menus:res.data.message
+        })
+      }
+    })
+  },
+  // 获取首页楼层数据
+   getfloorData(){
+     wx.request({
+       url:"https://api.zbztb.cn/api/public/v1/home/floordata",
+       success:res=>{
+        //  console.log(res)
+         this.setData({
+           floors:res.data.message
+         })
+       }
+     })
+   },
+  // 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    
+    wx.request({
+      url:"https://api.zbztb.cn/api/public/v1/home/swiperdata",
+      success:res=>{
+        // console.log(res)
+        // 把变量放在setData中,赋值后可以自动更新页面
+        this.setData({
+          images:res.data.message
+        })
+      }
+    })
+    this.getmenu()
+    this.getfloorData()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-    
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-    
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-    
   }
 })
